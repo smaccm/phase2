2dca6962cac5ba69caf8910564a8de17fd6b43d3
Sanjai <rsanjai@cs.umn.edu>
2015-05-11 16:07:54 -0500

    Updated Code-gen CLI corresponding to commit  e915a7f on smaccm/smaccm develop branch

--- ramses-demo/build_demo.sh
+++ ramses-demo/build_demo.sh
@@ -7,4 +7,4 @@ export RAMSES_DIR=../ramses_resource
 export AADL2RTOS_CONFIG_DIR=../aadl2rtos_resource
 
 
-java -jar ../ramses.jar -g rtos -i src -o out -l trace -s test1.impl -m SMACCM_SYS.aadl,test1.aadl
+java -jar ../ramses.jar -g rtos -i src,../aadl2rtos_resource  -o out -l trace -s test1.impl -m SMACCM_SYS.aadl,test1.aadl
