14770faa48073bdda1b3cad91c024a39372c95c0
Andrew Gacek <andrew.gacek@gmail.com>
2016-03-28 14:52:12 -0500

    Fix stack path for non-travis build

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -76,7 +76,6 @@ echo "Install stack"
 echo "************************************************************"
 mkdir -p $PWD/.local/bin
 curl -L https://www.stackage.org/stack/linux-x86_64 | tar xz --wildcards --strip-components=1 -C $PWD/.local/bin '*/stack'
-echo "$PWD/.local/bin:$PATH" >PATH
 
 echo "************************************************************"
 echo "Install gcc-arm-embedded"
@@ -84,4 +83,5 @@ echo "************************************************************"
 
 mkdir -p gcc-arm-embedded
 wget https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q2-update/+download/gcc-arm-none-eabi-4_9-2015q2-20150609-linux.tar.bz2 -O- | tar xjf - -C gcc-arm-embedded
-echo "$PWD:$(echo $PWD/gcc-arm-embedded/*/bin):/opt/ghc/7.8.4/bin:/opt/cabal/1.22/bin:/opt/alex/3.1.4/bin:/opt/happy/1.19.5/bin:$PATH" >PATH
+
+echo "$PWD/.local/bin:$(echo $PWD/gcc-arm-embedded/*/bin):/opt/ghc/7.8.4/bin:/opt/cabal/1.22/bin:/opt/alex/3.1.4/bin:/opt/happy/1.19.5/bin:$PATH" >PATH
